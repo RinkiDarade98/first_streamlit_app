b3acfdaf6af92d20025bb4ba6442bba1d24b4011
RinkiDarade98 <129477415+RinkiDarade98@users.noreply.github.com>
2023-11-08 16:32:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,4 +58,6 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add','Jackfrui
 streamlit.write('Thanks for adding ', add_my_fruit)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values('from streamlit')")
    
